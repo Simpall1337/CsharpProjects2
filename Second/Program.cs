2df3d535ddfd099a8bd20c8d67e1fad4ca3544db
Alex Huzo <sanduguzo1337@gmail.com>
2022-09-19 00:28:36 +0300

    .vs/

---
+++ Second/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть висоту трикутника : ");
+            int height = int.Parse(Console.ReadLine());
+            for (int i = 0; i < height; i++)
+            {
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < height; i++)
+            {
+
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < height; i++)
+            {
+
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
